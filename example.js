320197302f4d896c4ac13a5f3956b6211b484d54
Matheus Motta <matheusmotta.mm@gmail.com>
2014-12-06 14:51:41 -0200

    messagard done

--- example.js
+++ example.js
@@ -1,19 +1,12 @@
-var ExampleApplication = React.createClass({
+var MessageCard = React.createClass({
   render: function() {
-    var elapsed = Math.round(this.props.elapsed  / 100);
-    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-    var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
+    var date = this.props.date;
+  
+    var message = this.props.message;
 
-    return <p>{message}</p>;
+    return <div><span>{date}</span><br/><span>{message}</span></div>;
   }
 });
 
-var start = new Date().getTime();
+React.render(<MessageCard  date="21/12/2004" message="booooo" />, document.getElementById('container'));
 
-setInterval(function() {
-  React.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
-  );
-}, 50);
